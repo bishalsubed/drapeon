d7cf8b10af12feb3184d424220a2ac248eb1cfed
bishalsubed <142403372+bishalsubed@users.noreply.github.com>
2025-07-07 23:17:21 +0545

    get order by contact route added

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
-import { getAllOrders, changeOrderStatus, deleteOrder, getOrderById, getUserOrders } from "../controllers/order.controller.js";
+import { getAllOrders, changeOrderStatus, deleteOrder, getOrderById, getUserOrders, getOrderByContact } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get("/of-user", protectRoute,getUserOrders)
 router.get("/id/:orderId", protectRoute,getOrderById)
 router.patch("/:orderId", protectRoute, adminRoute,changeOrderStatus)
 router.delete("/:orderId", protectRoute, adminRoute,deleteOrder)
+router.get("/searchOrder", getOrderByContact);
 
 export default router;
